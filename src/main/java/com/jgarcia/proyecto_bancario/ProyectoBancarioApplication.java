b7cd8f360ad3c44b094db26181b0142cca577072
josegarcia19809 <josegarcia19809@gmail.com>
2024-09-22 07:17:15 -0600

    Creando el proyecto Spring

---
+++ src/main/java/com/jgarcia/proyecto_bancario/ProyectoBancarioApplication.java
@@ -0,0 +1,13 @@
+package com.jgarcia.proyecto_bancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBancarioApplication.class, args);
+	}
+
+}
